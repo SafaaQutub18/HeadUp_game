3e19d3fc53d844097842351ce7e673b4fd1e64e6
Safaa <safaaqutub@gmail.com>
2021-12-15 00:01:43 +0300

    Add timer

--- app/src/main/java/com/example/headup_game/GameActivity.kt
+++ app/src/main/java/com/example/headup_game/GameActivity.kt
@@ -2,10 +2,51 @@ package com.example.headup_game
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Toast
+import com.example.headup_game.databinding.ActivityGameBinding
 
 class GameActivity : AppCompatActivity() {
+    lateinit var binding : ActivityGameBinding
+
+    //timer var:
+    private lateinit var countDownTimer : CountDownTimer
+    private var timeLeftInMilliSec :Long = 60000 // = 1 min
+    private var timerRunning = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_game)
+        binding = ActivityGameBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        getCelebrity()
+        startTimer()
+
+    }
+
+    private fun startTimer() {
+
+        countDownTimer = object: CountDownTimer(timeLeftInMilliSec, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                timeLeftInMilliSec = millisUntilFinished
+                val seconds : Int = (timeLeftInMilliSec % 60000 / 1000).toInt()
+                binding.timerTv.text = "00:$seconds"
+            }
+
+            override fun onFinish() {
+                Toast.makeText(this@GameActivity,"end timer",Toast.LENGTH_SHORT).show()
+            }
+
+        }
+        countDownTimer.start()
+        timerRunning = true
+    }
+    fun stopTimer(){
+        countDownTimer.cancel()
+        timerRunning = false
+    }
+
+    private fun getCelebrity() {
+
     }
 }
